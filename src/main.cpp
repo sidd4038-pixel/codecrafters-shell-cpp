79b0de6ae0f92e3cded1c08ae978d1052cb59986
Siddharth Patel <siddharthpatel@Siddharths-MacBook-Air.local>
2025-11-08 02:38:57 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,19 @@ int main()
       exit(0);
     }
     else if(line.substr(0,4) == "echo"){
-      std::cout << line.substr(5) << std::endl;
+      std::cout << line.substr(5) << '\n';
       continue;
     }
-    std::cout << line << ": command not found" << std::endl;
+    else if(line.substr(0,4) == "type"){
+      if(line.substr(5) == "echo" || line.substr(5) == "exit" || line.substr(5) == "type"){
+        std::cout << line.substr(5) << " is a shell builtin" << '\n';
+      }
+      else std::cout << line.substr(5) << ": not found" << '\n';
+      continue;
+    }
+    else{
+      std::cout << line << ": command not found" << '\n';
+    }
   }
 
   return 0;
